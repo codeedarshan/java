a13f82b6d2567141bd679ada7ff2d81b46d66ca3
codeedarshan <darshanmlsu@gmail.com>
2026-06-09 22:58:42 +0530

    Add files via upload
    vote

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int age = 10;
+
+        if (age >= 18) 
+            {
+            System.out.println("eligible to vote");
+            }
+            else 
+            {
+            System.out.println("NOT eligible to vote");
+            }
+    }
+}
